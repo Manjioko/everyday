f2a712f00a87ccec847a6323799a8311129e614a
燕子的macbook <swallow@yanzideMacbook-Air.local>
2022-08-09 21:22:37 +0800

    node 页面完成绘制

--- src/views/NodePage.vue
+++ src/views/NodePage.vue
@@ -1,52 +1,71 @@
 <template>
-    <header class="node-header">
-        <section class="nh-s">
-            <article class="nh-contains">
-                <div class="nh-logo">
-                    <img src="https://nodejs.org/static/images/logo.svg" alt="" class="nh-logo-img">
-                </div>
-                <div class="nh-nav">
-                    <ul class="nh-nav-ul">
-                        <li class="nh-nav-li" v-for="(item, index) in navBox" :key="index">
-                            <a href="" class="nh-nav-a">{{item}}</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="nh-trans">
-                    <div class="nh-night"></div>
-                    <div class="nh-t"></div>
-                </div>
-            </article>
-        </section>
-    </header>
-
-    <main class="node-main">
-        <section class="nm-s">
-            <article class="nm-contains">
-                <h3 class="nm-h3">Node.js® 是一个基于 <a href="" class="nm-a">Chrome V8 引擎</a> 的 JavaScript 运行时环境。</h3>
-                <p class="nm-patch-p"><a href="" class="nm-patch-a">Node.js 14，16 和 18 版本的安全补丁已发布</a></p>
-                <h1 class="nm-h1">下载平台为： Windows (x64)</h1>
-
-                <div class="nm-list">
-                    <div class="nm-box" v-for="(item,index) in greenBox" :key="index">
-                        <div class="nm-green-sub">
-                            <h1 class="nm-g-s-h1">{{item.title}}</h1>
-                            <p class="nm-g-s-p">{{item.sub}}</p>
-                        </div>
-                        <ul class="nm-g-bottom-ul">
-                            <li class="nm-g-bottom-li" v-for="(subItem,subIndex) in item.list" :key="subIndex">
-                                <a href="" class="nm-g-b-a">{{subItem}}</a>
+    <article class="node-layout">
+        <header class="node-header">
+            <section class="nh-s">
+                <article class="nh-contains">
+                    <div class="nh-logo">
+                        <img src="https://nodejs.org/static/images/logo.svg" alt="" class="nh-logo-img">
+                    </div>
+                    <div class="nh-nav">
+                        <ul class="nh-nav-ul">
+                            <li class="nh-nav-li" v-for="(item, index) in navBox" :key="index">
+                                <a href="" class="nh-nav-a">{{item}}</a>
                             </li>
                         </ul>
                     </div>
-                </div>
+                    <div class="nh-trans">
+                        <div class="nh-night"></div>
+                        <div class="nh-t"></div>
+                    </div>
+                </article>
+            </section>
+        </header>
 
-                <h2 class="nm-tip">可参考 <a href="" class="nm-tip-a"> 长期维护版（LTS）日程</a></h2>
+        <main class="node-main">
+            <section class="nm-s">
+                <article class="nm-contains">
+                    <h3 class="nm-h3">Node.js® 是一个基于 <a href="" class="nm-a">Chrome V8 引擎</a> 的 JavaScript 运行时环境。</h3>
+                    <p class="nm-patch-p"><a href="" class="nm-patch-a">Node.js 14，16 和 18 版本的安全补丁已发布</a></p>
+                    <h1 class="nm-h1">下载平台为： Windows (x64)</h1>
 
-                
-            </article>
-        </section>
-    </main>
+                    <div class="nm-list">
+                        <div class="nm-box" v-for="(item,index) in greenBox" :key="index">
+                            <div class="nm-green-sub">
+                                <h1 class="nm-g-s-h1">{{item.title}}</h1>
+                                <p class="nm-g-s-p">{{item.sub}}</p>
+                            </div>
+                            <ul class="nm-g-bottom-ul">
+                                <li class="nm-g-bottom-li" v-for="(subItem,subIndex) in item.list" :key="subIndex">
+                                    <a href="" class="nm-g-b-a">{{subItem}}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h2 class="nm-tip">可参考 <a href="" class="nm-tip-a"> 长期维护版（LTS）日程</a></h2>
+                </article>
+            </section>
+        </main>
+
+        <footer class="node-footer">
+            <section class="nf-s">
+                <article class="ns-contains">
+                    <p class="ns-contents">
+                        Copyright <a href="" class="ns-openjs">OpenJS Foundation</a> and Node.js contributors. All rights reserved.
+                        The <a href="" class="ns-openjs">OpenJS Foundation</a> has registered trademarks and uses trademarks. 
+                        For a list of trademarks of the <a href="" class="ns-openjs">OpenJS Foundation</a>, please see our
+                        <a href="" class="ns-openjs">Trademark Policy</a> and <a href="" class="ns-openjs">Trademark List.</a> 
+                        Trademarks and logos not indicated on the list of <a href="" class="ns-openjs">OpenJS Foundation trademarks</a>  are trademarks™ or registered® trademarks of their respective holders.
+                        Use of them does not imply any affiliation with or endorsement by them.
+                    </p>
+                    <ul class="ns-ul">
+                        <li class="ns-li" v-for="(item,index) in footerBox" :key="index">
+                            <a href="" class="ns-a">{{item}}</a>
+                        </li>
+                    </ul>
+                </article>
+            </section>
+        </footer>
+    </article>
 </template>
 
 <script lang="ts">
@@ -65,7 +84,9 @@ export default defineComponent({
             greenBox: [
                 {title:'16.16.0 长期维护版',sub:'推荐多数用户使用',list:['其它下载','更新日志','API 文档']},
                 {title:'18.7.0 最新尝鲜版',sub:'含最新功能',list:['其它下载','更新日志','API 文档']},
-            ]
+            ],
+            footerBox: ['The OpenJS Foundation','Terms of Use','Privacy Policy','Bylaws','Code of Conduct',
+            'Trademark Policy','Trademark List','Cookie Policy','在 GitHub 上编辑']
         }
     }
 
@@ -198,10 +219,14 @@ export default defineComponent({
 }
 
 .nm-list {
-    display: grid;
-    grid-template-columns: repeat(2,1fr);
-    column-gap: 26px;
+    display: flex;
+    justify-content: center;
 }
+
+.nm-box {
+    padding: 0 5px;
+}
+
 .nm-green-sub {
     background-color: #026e00;
     margin: 10px 4px;
@@ -252,5 +277,63 @@ export default defineComponent({
 .nm-g-b-a {
     color: #51744e;
     text-decoration: none;
+}
+.node-layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.node-main {
+    flex: 1;
+}
+
+.node-footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    position: relative;
+}
+
+.ns-contains {
+    max-width: 980px;
+    margin: 0 auto;
+    display: inline-block;
+    padding: 1rem 20px;
+}
+
+.ns-contents {
+    font-size: small;
+}
+
+.ns-openjs {
+    color: #add8e6;
+    text-decoration: none;
+}
+.ns-ul {
+    margin-bottom: 0;
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    flex-wrap: nowrap;
+}
+
+.ns-li:not(:first-child)::before {
+    content: "|";
+    color: #fff;
+    padding: 1.5px;
+}
+
+.ns-a {
+    color: #add8e6;
+    text-decoration: none;
+    font-size: 15px;
+    white-space: nowrap;
+    font-weight: 600;
+}
+
+.ns-li {
+    white-space: nowrap;
 }
 </style>
